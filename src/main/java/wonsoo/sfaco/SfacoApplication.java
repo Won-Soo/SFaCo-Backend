05c01e6880720983a72459bef40e947b58b2fabd
Won-dy <junhfireace@gmail.com>
2022-03-07 10:37:14 +0900

    Spring Boot, MySQL 개발 환경 구축

---
+++ src/main/java/wonsoo/sfaco/SfacoApplication.java
@@ -0,0 +1,13 @@
+package wonsoo.sfaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wonsoo/sfaco/SfacoApplicationTests.java
@@ -0,0 +1,13 @@
+package wonsoo.sfaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
